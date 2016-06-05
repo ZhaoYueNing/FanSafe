b95a2f7d8ed3f4952d6425ffe909521f9ec1c086
Buynow <zhaozhaozhao1996@163.com>
2016-06-05 10:21:25 +0800

    完成版本检测 与更新

---
+++ app/src/main/java/com/buynow/fansafe/activity/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.buynow.fansafe.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by buynow on 16-6-4.
+ * App 主界面
+ */
+public class HomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
